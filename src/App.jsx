fbdf6f860d70ce73835035fd4c798830ee6b490b
Mark Zhdan <markzhdan@gmail.com>
2025-04-16 11:26:06 -0500

    feat: added example component page

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
 import "./App.css";
 import splash from "./assets/UICircle_Full.png";
+import MainPage from "./pages/MainPage";
 
 function App() {
   return (
     <div className="App">
-      <img src={splash} alt="UICircle splash logo" height={300} />
+      <img src={splash} alt="UICircle splash logo" height={300} width={400} />
+      <MainPage />
     </div>
   );
 }
---
+++ src/pages/MainPage.jsx
@@ -0,0 +1,11 @@
+import "./MainPage.css";
+
+function MainPage() {
+  return (
+    <div className="MainPage">
+      <h1>MainPage</h1>
+    </div>
+  );
+}
+
+export default MainPage;
