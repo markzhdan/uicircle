6220836656f2c9ce8af9d31c77e4410be7ae86e5
Mallika Patil <43118516+mallikapatil00@users.noreply.github.com>
2025-04-21 22:16:11 -0500

    updated the main page 04/21/25

--- src/pages/MainPage.jsx
+++ src/pages/MainPage.jsx
@@ -1,10 +1,18 @@
 import "./MainPage.css";
 
 function MainPage() {
+  // return (
+  //   <div className="MainPage">
+  //     <h1>MainPage</h1>
+  //   </div>
+  // );
+  // return <h1>hello</h1>;
   return (
-    <div className="MainPage">
-      <h1>MainPage</h1>
+    <div class="MainPage">
+      <div class="intro_text">Immerse into the pixelated flames of UIC, and embark on a path to sealing your future success</div>
+      <button class="intro_button"> click here </button>
     </div>
+    
   );
 }
 
