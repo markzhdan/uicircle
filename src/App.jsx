d62bc3c3a3e39d3b89d5a10b54905d7baccc8a8e
Mallika Patil <43118516+mallikapatil00@users.noreply.github.com>
2025-04-22 17:22:09 -0500

    new niharika change

--- src/App.jsx
+++ src/App.jsx
@@ -11,4 +11,29 @@ function App() {
   );
 }
 
-export default App;
+// export default App;
+
+// import "./App.css";
+// import Layout from "./components/Layout";
+// import MainPage from "./pages/MainPage";
+// import Messages from "./pages/Messages";
+
+// import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+
+// function App() {
+//   return (
+//     <Router>
+//       <div className="App">
+//         {/* iPhone layout here */}
+//         <Layout>
+//           <Routes>
+//             <Route path="/" element={<MainPage />} />
+//             <Route path="/messages" element={<Messages />} />
+//           </Routes>
+//         </Layout>
+//       </div>
+//     </Router>
+//   );
+// }
+
+// export default App;
