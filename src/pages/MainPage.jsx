c9769044b637c2ac79f09eaa2058e3ba08012a87
Mallika Patil <43118516+mallikapatil00@users.noreply.github.com>
2025-04-22 17:31:53 -0500

    recent niharika changes

--- src/pages/MainPage.jsx
+++ src/pages/MainPage.jsx
@@ -1,19 +1,34 @@
 import "./MainPage.css";
+import { Link } from "react-router-dom";
 
 function MainPage() {
-  // return (
-  //   <div className="MainPage">
-  //     <h1>MainPage</h1>
-  //   </div>
-  // );
-  // return <h1>hello</h1>;
   return (
-    <div class="MainPage">
+    <main class="MainPage">
       <div class="intro_text">Immerse into the pixelated flames of UIC, and embark on a path to sealing your future success</div>
       <button class="intro_button"><p class="button_text">Create Account</p></button>
-    </div>
+      <Link to="/messages">
+//      <button type="button">To Messages</button>
+//    </Link>
+//  </main>
     
   );
 }
 
 export default MainPage;
+
+//import "./MainPage.css";
+//import { Link } from "react-router-dom";
+
+// function MainPage() {
+//   return (
+//     <main className="MainPage">
+//       <h1>Put the main page here</h1>
+
+//       <Link to="/messages">
+//         <button type="button">To Messages</button>
+//       </Link>
+//     </main>
+//   );
+// }
+
+// export default MainPage;
