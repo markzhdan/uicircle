828f0cb82f2c4d2c176dcff302a6570a1f455eac
Mallika Patil <43118516+mallikapatil00@users.noreply.github.com>
2025-04-23 16:56:49 -0500

    starting profile builder

--- src/pages/Profile.jsx
+++ src/pages/Profile.jsx
@@ -1,12 +1,11 @@
 import "./Profile.css";
-
+import dragon_img from "../assets/UICircle_Icon.png";
+import { Link } from "react-router-dom";
 function Profile() {
   return (
     <div className="Profile">
-      <h1>Messages!</h1>
-
-      <img src="" alt="" />
-      <p>text</p>
+      <img className="img" src={dragon_img} height="200px" width="200px"></img>
+      <h1 className="title">Who Are You?</h1>
     </div>
   );
 }
