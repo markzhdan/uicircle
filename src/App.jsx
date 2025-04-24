48cac25a1ad0d1d737d1750d98b713daf55c2907
Akona04 <129303298+Akona04@users.noreply.github.com>
2025-04-24 12:02:31 -0500

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,22 +1,24 @@
-import "./App.css";
-import Layout from "./components/Layout";
-import MainPage from "./pages/MainPage";
-import Messages from "./pages/Messages";
-
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import React from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Layout from './components/Layout';
+import FavClubs from './pages/FavClubs';
+import ClubDetail from './pages/ClubDetail';
+import './App.css';
 
 function App() {
   return (
     <Router>
-      <div className="App">
-        {/* iPhone layout here */}
-        <Layout>
-          <Routes>
-            <Route path="/" element={<MainPage />} />
-            <Route path="/messages" element={<Messages />} />
-          </Routes>
-        </Layout>
-      </div>
+      <Routes>
+        <Route path="/" element={<Layout />}>
+          <Route index element={<FavClubs />} />
+          <Route path="/fav-clubs" element={<FavClubs />} />
+          <Route path="/home" element={<div>Home Placeholder</div>} />
+          <Route path="/calendar" element={<div>Calendar Placeholder</div>} />
+          <Route path="/messages" element={<div>Messages Placeholder</div>} />
+          <Route path="/search" element={<div>Search Clubs Placeholder</div>} />
+          <Route path="/club/:id" element={<ClubDetail />} />
+        </Route>
+      </Routes>
     </Router>
   );
 }
