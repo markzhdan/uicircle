65fb85efa28912ee361dcb957aa7dfc90b1c7ee6
Mark Zhdan <markzhdan@gmail.com>
2025-04-25 23:39:52 -0500

    feat: link msg icon to messages tab

--- src/pages/ClubDetail.jsx
+++ src/pages/ClubDetail.jsx
@@ -1,14 +1,14 @@
-import React from 'react';
-import { useParams, useNavigate, NavLink } from 'react-router-dom';
-import './ClubDetail.css';
-import favClubsIcon from '../assets/fav-clubs.png';
-import homeIcon from '../assets/home.png';
-import calendarIcon from '../assets/calendar.png';
-import messagesIcon from '../assets/messages.png';
-import backIcon from '../assets/Group.svg';
-import pmrigIcon from '../assets/pmrig.png';
-import csIcon from '../assets/cs.png';
-import historyIcon from '../assets/history.png';
+import React from "react";
+import { useParams, useNavigate, NavLink } from "react-router-dom";
+import "./ClubDetail.css";
+import favClubsIcon from "../assets/fav-clubs.png";
+import homeIcon from "../assets/home.png";
+import calendarIcon from "../assets/calendar.png";
+import messagesIcon from "../assets/messages.png";
+import backIcon from "../assets/Group.svg";
+import pmrigIcon from "../assets/pmrig.png";
+import csIcon from "../assets/cs.png";
+import historyIcon from "../assets/history.png";
 
 function ClubDetail() {
   const { id } = useParams();
@@ -17,47 +17,49 @@ function ClubDetail() {
   const clubs = [
     {
       id: 1,
-      name: 'PMRIG',
-      description: 'Serves to inform students about critical resources to learn about residency and future careers',
+      name: "PMRIG",
+      description:
+        "Serves to inform students about critical resources to learn about residency and future careers",
       icon: pmrigIcon,
       events: [
-        { date: 23, title: 'GBM' },
-        { date: 17, title: 'Intro' },
+        { date: 23, title: "GBM" },
+        { date: 17, title: "Intro" },
       ],
       members: [
-        { name: 'Juliette - President', hasMessage: true },
-        { name: 'Sandra', hasMessage: false },
-        { name: 'Preethi', hasMessage: false },
-      ]
+        { name: "Juliette - President", hasMessage: true },
+        { name: "Sandra", hasMessage: false },
+        { name: "Preethi", hasMessage: false },
+      ],
     },
     {
       id: 2,
-      name: 'CS Club',
-      description: 'Connects students with like-minded inviduals with common interests to create fun projects',
+      name: "CS Club",
+      description:
+        "Connects students with like-minded inviduals with common interests to create fun projects",
       icon: pmrigIcon,
       events: [
-        { date: 10, title: 'GBM' },
-        { date: 24, title: 'Career Talk' },
+        { date: 10, title: "GBM" },
+        { date: 24, title: "Career Talk" },
       ],
       members: [
-        { name: 'Sandra', hasMessage: false },
-        { name: 'Preethi', hasMessage: false },
-      ]
+        { name: "Sandra", hasMessage: false },
+        { name: "Preethi", hasMessage: false },
+      ],
     },
     {
       id: 3,
-      name: 'History',
-      description: 'Romeo, Romeo, where are thou',
+      name: "History",
+      description: "Romeo, Romeo, where are thou",
       icon: pmrigIcon,
       events: [
-        { date: 14, title: 'GBM' },
-        { date: 16, title: 'Meet Greet' },
+        { date: 14, title: "GBM" },
+        { date: 16, title: "Meet Greet" },
       ],
       members: [
-        { name: 'Sandra', hasMessage: false },
-        { name: 'Preethi', hasMessage: false },
-      ]
-    }
+        { name: "Sandra", hasMessage: false },
+        { name: "Preethi", hasMessage: false },
+      ],
+    },
   ];
 
   const club = clubs.find((club) => club.id === parseInt(id, 10));
@@ -70,7 +72,12 @@ function ClubDetail() {
     <div className="club-detail-container">
       <main className="club-detail-content">
         <div className="pink-container">
-          <img src={backIcon} alt="Back" className="back-icon" onClick={() => navigate('/fav-clubs')} />
+          <img
+            src={backIcon}
+            alt="Back"
+            className="back-icon"
+            onClick={() => navigate("/fav-clubs")}
+          />
 
           <div className="club-header">
             <h2 className="club-title">{club.name}</h2>
@@ -95,13 +102,15 @@ function ClubDetail() {
             ))}
           </div>
 
-
           <div className="members">
             {club.members.map((member, index) => (
               <div className="member" key={index}>
                 <span className="member-icon">👤</span>
                 <span className="member-name">{member.name}</span>
-                <span className="message-icon">💬</span>
+
+                <NavLink to="/messages" style={{ textDecoration: "none" }}>
+                  <span className="message-icon">💬</span>
+                </NavLink>
               </div>
             ))}
           </div>
@@ -109,20 +118,20 @@ function ClubDetail() {
       </main>
       <nav className="bottom-nav">
         <NavLink to="/fav-clubs" className="nav-item">
-            <img src={favClubsIcon} alt="Fav Clubs" />
-            <span>Fav Clubs</span>
+          <img src={favClubsIcon} alt="Fav Clubs" />
+          <span>Fav Clubs</span>
         </NavLink>
         <NavLink to="/home" className="nav-item">
-            <img src={homeIcon} alt="Home" />
-            <span>Home</span>
+          <img src={homeIcon} alt="Home" />
+          <span>Home</span>
         </NavLink>
         <NavLink to="/calendar" className="nav-item">
-            <img src={calendarIcon} alt="Calendar" />
-            <span>Calendar</span>
+          <img src={calendarIcon} alt="Calendar" />
+          <span>Calendar</span>
         </NavLink>
         <NavLink to="/messages" className="nav-item">
-            <img src={messagesIcon} alt="Messages" />
-            <span>Messages</span>
+          <img src={messagesIcon} alt="Messages" />
+          <span>Messages</span>
         </NavLink>
       </nav>
     </div>
